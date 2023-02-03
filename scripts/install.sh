94eb012afbd4cabe976efdb0d787a57eff4a1a0c
Filip Izydorczyk <filip.izydorczyk@protonmail.com>
2023-02-03 01:21:06 +0100

    dev: creting home directories (default and custom ones)

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,6 +2,24 @@
 
 # Run it from project root directory
 
+echo "Installing ohmyzsh"
+
 sh ./vendor/ohmyzsh/tools/install.sh
 
+echo "Installing scripts"
+
 cp -f ./lib/* ~/.local/scripts/
+
+echo "Installing directory structure"
+
+mkdir -p ~/Backups
+mkdir -p ~/Contracts
+mkdir -p ~/Development
+mkdir -p ~/Files
+mkdir -p ~/Tests
+
+mkdir -p ~/Documents
+mkdir -p ~/Downloads
+mkdir -p ~/Music
+mkdir -p ~/Pictures
+mkdir -p ~/Videos

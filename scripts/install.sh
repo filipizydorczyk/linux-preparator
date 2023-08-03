0233877504821c1dd33d0f3b06a09325259052e3
Filip Izydorczyk <filip.izydorczyk@protonmail.com>
2023-08-03 20:31:26 +0200

    dev: missing installoing instructions

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,13 +2,29 @@
 
 # Run it from project root directory
 
+# Needs to be prepared
+# echo "Installing pacman packages"
+
+# echo "Installing external utilites"
+
+# git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+# git clone https://github.com/meiokubo-zz/emoji.zsh-theme.git
+# cp emoji.zsh-theme ~/.oh-my-zsh/themes
+
+# git clone https://aur.archlinux.org/yay.git
+# makepkg -si
+
+# echo "Installing AUR packages"
+
 echo "Installing ohmyzsh"
 
 sh ./vendor/ohmyzsh/tools/install.sh
 
 echo "Installing scripts"
 
+mkdir -p ~/.local/scripts/
 cp -f ./bin/* ~/.local/scripts/
+chmod +x ~/.local/scripts/*
 
 echo "Installing directory structure"
 
